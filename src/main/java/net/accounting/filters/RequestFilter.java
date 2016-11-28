16d4697727c4218d2a5034720e26d01bf3a515c5
Vitalii Nedzelskyi <vitalii.nedzelskyi@gmail.com>
2016-11-28 10:29:38 +0200

    Added Time displaying for request

--- src/main/java/net/accounting/filters/RequestFilter.java
+++ src/main/java/net/accounting/filters/RequestFilter.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Admin on 14-Apr-16.
@@ -14,6 +16,8 @@ public class RequestFilter extends BaseFilter {
 
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
+        String pattern = " dd-MM-yyyy HH:mm:ss";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
         String protocol = request.getProtocol();
         String method = request.getMethod();
         String queryString = request.getQueryString();
@@ -23,6 +27,7 @@ public class RequestFilter extends BaseFilter {
                 + "Method: " + method + "\n"
                 + "QueryString: " + queryString + "\n"
                 + "IP: " + ip + "\n"
+                + "Time" + dateFormat.format(new Date()) + "\n"
                 + "The end of request  <<<<<<");
 
         chain.doFilter(request, response);
